044d32b2c206626956bbcfbd968e6b94481fc63e
tauseef-khan <tdakhan@gmail.com>
2020-04-15 21:21:41 +0100

    getAlbum returns JSON result

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -7,7 +8,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-
+  getAlbum(id: number): Observable<Response> {
+    return this._http.get(this._albumUrl);
   }
 }
