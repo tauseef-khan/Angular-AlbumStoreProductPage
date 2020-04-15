f54eb82ad6b93ee12629b0dec57bd15af4bc029e
tauseef-khan <tdakhan@gmail.com>
2020-04-15 21:57:58 +0100

    after comparing solutions fixed the issue of mapping the JSON response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -9,7 +9,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(
-      map(response => response.json()));
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 }
