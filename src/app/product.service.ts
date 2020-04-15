8fe576e1dbc9027360f126183aed1e6e0e9c3e14
tauseef-khan <tdakhan@gmail.com>
2020-04-15 21:54:36 +0100

    attempt at mapping JSON response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -9,6 +9,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl);
+    return this._http.get(this._albumUrl).pipe(
+      map(response => response.json()));
   }
 }
