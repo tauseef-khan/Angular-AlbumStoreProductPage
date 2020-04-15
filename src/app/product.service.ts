827421c8b09439a83e7932ccc40072710db8a6fa
tauseef-khan <tdakhan@gmail.com>
2020-04-15 21:17:11 +0100

    imported http and response classes & injected private http object & added album url variable & added getAlbum method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class ProductService {
+  _albumUrl: string = '../assets/album.json';
 
-  constructor() { }
+  constructor(private _http: Http) { }
 
+  getAlbum(id: number) {
+
+  }
 }
